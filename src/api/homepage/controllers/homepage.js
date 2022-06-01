92fd6e92e0e05f5399ceafe938a1d22647fd5591
Jorge Simoes <jorgehsy@gmail.com>
2022-06-01 00:00:45 -0400

    some changes in the api

--- src/api/homepage/controllers/homepage.js
+++ src/api/homepage/controllers/homepage.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::homepage.homepage');
+module.exports = createCoreController('api::homepage.homepage', ({ strapi }) => ({
+    async find(ctx) {
+        const populateList = [
+            'integralService.*',
+            'advantagesList.*',
+            'promoSection.*',
+            'seo.*'
+        ]
+        // Push any additional query params to the array
+        populateList.push(ctx.query.populate)
+        ctx.query.populate = populateList.join(',')
+
+        const content = await super.find(ctx)
+        return content
+    }
+}));
